a49053519604e15025fd961d8ce0312050ac1110
NathanVanR <140237302+NathanVanR@users.noreply.github.com>
2026-06-05 19:21:43 +0200

    initial commit

---
+++ src/main/java/com/yieldshare/YieldshareApplication.java
@@ -0,0 +1,13 @@
+package com.yieldshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YieldshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YieldshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yieldshare/YieldshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yieldshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YieldshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
